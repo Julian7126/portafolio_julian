9b6bb4ad7fcb22728b37fefafe8fef1005ee22e6
julianbischoff <julibischoff@gmail.com>
2023-11-27 15:41:56 -0600

    fixVercel

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -3,12 +3,7 @@
 const AboutMe = () => {
   return (
     <div>
-    //hacer un contenedor para un texto que tenga alguna libreria minimalista que muestre el texto apareciendo cuando se muestra el contenedor en pantalla
-
-
-
-
-
+      about me 
         
     </div>
   )
