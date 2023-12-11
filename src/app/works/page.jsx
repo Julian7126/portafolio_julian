92d014892f8a613c3eae1bab143b4668283a485a
julianbischoff <julibischoff@gmail.com>
2023-12-11 11:03:46 -0600

    cahnges2

--- src/app/works/page.jsx
+++ src/app/works/page.jsx
@@ -5,7 +5,7 @@ dotenv.config();
 
 
 async function loadProyects() {
-  const response = await fetch("/api/proyects",{ next: { revalidate: 3600 } }) ;
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/proyects",{ next: { revalidate: 3600 } }) ;
   const datanew = await response.json();
   console.log(datanew);
   return datanew;
